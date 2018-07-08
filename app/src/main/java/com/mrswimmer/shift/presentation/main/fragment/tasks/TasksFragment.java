e72512fd00c0856d87a15c1c465a05704513efb8
mrswimmer <mrswimmerlab@gmail.com>
2018-07-08 12:42:32 +0700

    add gs to gi

--- app/src/main/java/com/mrswimmer/shift/presentation/main/fragment/tasks/TasksFragment.java
+++ app/src/main/java/com/mrswimmer/shift/presentation/main/fragment/tasks/TasksFragment.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.mrswimmer.shift.App;
 import com.mrswimmer.shift.R;
 import com.mrswimmer.shift.data.model.firebase.Task;
-import com.mrswimmer.shift.di.utils.TaskDiffUtilCallback;
+import com.mrswimmer.shift.domain.utils.TaskDiffUtilCallback;
 import com.mrswimmer.shift.presentation.base.BaseFragment;
 import com.mrswimmer.shift.presentation.main.fragment.tasks.recycler.TaskAdapter;
 
